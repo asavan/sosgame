3efcf29ea6e66cadcccfed9ba3577dd20935ecd0
a.savanovich <asavanster@gmail.com>
2024-05-22 20:38:48 +0300

    random tests

--- tests/bot/random.test.js
+++ tests/bot/random.test.js
@@ -19,3 +19,40 @@ test("normal move", () => {
     assert.equal(fieldObj.NORMAL_MOVE, result.res);
     assert.ok(2 <= result.position);
 });
+
+test("simple move", () => {
+    const field = fieldObj.field([5, 0, 0, 5, 0]);
+    assert.equal(5, field.size());
+    const result = bot.bestMove(field);
+    assert.equal(fieldObj.NORMAL_MOVE, result.res);
+    assert.equal(4, result.position);
+});
+
+test("winning move2", () => {
+    const field = fieldObj.field([5, 0, 5, 5, 0]);
+    const result = bot.bestMove(field);
+    assert.equal(fieldObj.WINNING_MOVE, result.res);
+    assert.equal(1, result.position);
+    assert.equal(1, result.digit);
+});
+
+test("simple move3", () => {
+    const field = fieldObj.field([0, 5, 0, 0, 0]);
+    const result = bot.bestMove(field);
+    assert.equal(fieldObj.NORMAL_MOVE, result.res);
+    assert.notEqual(1, result.position);
+});
+
+test("simple move4", () => {
+    const field = fieldObj.field([5, 0, 0, 5]);
+    const result = bot.bestMove(field);
+    assert.equal(fieldObj.NORMAL_MOVE, result.res);
+    assert.ok([1, 2].includes(result.position), `${result.position}`);
+});
+
+test("notLoosingMove", () => {
+    const field = fieldObj.field([5, 0, 0, 5, 0, 2, 0, 0]);
+    const result = bot.bestMove(field);
+    assert.equal(fieldObj.NORMAL_MOVE, result.res);
+    assert.ok(4 <= result.position);
+});
