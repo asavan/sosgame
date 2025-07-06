b652cc22dfba6ef8f8e6e0aece5ab0aed2d9495c
asavan <asavan@mail.ru>
2025-07-06 21:04:26 +0300

    barcode

--- src/js/views/barcode.js
+++ src/js/views/barcode.js
@@ -1,23 +1,37 @@
 export default async function scanBarcode(logger, document) {
     try {
         logger.error("before media1");
-        const barcodeDetector = new BarcodeDetector({ formats: ["qr_code"] });
+        const barcodeDetector = new BarcodeDetector(/*{formats: ["qr_code"]}*/);
         logger.error("before media2");
-        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } });
+        const stream = await navigator.mediaDevices.getUserMedia({video: {facingMode: "environment"}});
         const video = document.createElement("video");
         video.srcObject = stream;
         document.querySelector(".video-barcode").appendChild(video);
         // video.autoplay = true;
         await video.play();
         logger.error("after media2");
-        const barcodes = await barcodeDetector.detect(video);
+        const codesPromice = Promise.withResolvers();
 
-        if (barcodes.length > 0) {
-            logger.log(barcodes);
-            // Process the detected barcodes
-            stream.getTracks().forEach(track => track.stop()); // Stop the camera when done
+        async function detect(codesPromice) {
+            const barcodes = await barcodeDetector.detect(video);
+            logger.error("after detect");
+
+            if (barcodes.length > 0) {
+                logger.log(barcodes);
+                // Process the detected barcodes
+                stream.getTracks().forEach(track => track.stop()); // Stop the camera when done
+                codesPromice.resolve(barcodes);
+            } else {
+                logger.error("No codes found");
+                requestAnimationFrame(() => {
+                    detect(codesPromice);
+                });
+            }
+            return codesPromice;
         }
-        return barcodes;
+
+        detect(codesPromice);
+        return codesPromice.promise;
     } catch (error) {
         logger.error("Error accessing camera or detecting barcodes:", error);
     }
