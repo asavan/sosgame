189b44284668659660ab9080a6bd67cf3718d521
a.savanovich <asavanster@gmail.com>
2025-10-09 09:51:26 +0300

    use sw from devdeps

--- webpack_configs/production.webpack.config.js
+++ webpack_configs/production.webpack.config.js
@@ -2,7 +2,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 import PACKAGE from "../package.json" with { type: "json" };
-import {webpackProd} from "netdeps";
+import {webpackProd} from "devdeps";
 
 const prodConfig = () => {
     const dirname = path.dirname(fileURLToPath(import.meta.url));
