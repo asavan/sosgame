28cd68bc1f7da8f8bccdadc59a8299c9551b2c41
a.savanovich <asavanster@gmail.com>
2026-09-05 08:06:25 +0300

    mdns work on windows

--- devtools/bonjur.js
+++ devtools/bonjur.js
@@ -2,9 +2,9 @@ import { Bonjour } from 'bonjour-service';
 const instance = new Bonjour();
 
 // Publish an HTTP service on port 3000 named "My Webserver"
-instance.publish({ name: 'ppp', type: 'http', port: 8080, host: 'ppp.local', disableIPv6: true });
+instance.publish({ name: 'sosgame', type: 'http', port: 8080, host: 'sosgame.local', disableIPv6: true });
 
 // Browse for all HTTP services
 instance.find({ type: 'http' }, (service) => {
-    console.log('Found HTTP service:', service.name, service.referer.address);
+    console.log('Found HTTP service:', service.name, service.host, service.referer.address);
 });
